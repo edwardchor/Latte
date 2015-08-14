f9057951b52fe808ac3ffb1fad472c2f91294f52
储仲尧 <czy8023@me.com>
2015-08-14 15:00:26 +0800

    Initialization of the latte.

---
+++ Latte/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Latte
+//
+//  Created by 储仲尧 on 8/14/15.
+//  Copyright © 2015 Uopia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
